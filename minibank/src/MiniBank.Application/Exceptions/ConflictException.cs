ac9bc4a49bfbf2b4cea874beef0fead9746dcf2c
HoangMh <hoangmhhe180845@fpt.edu.vn>
2026-03-10 12:54:12 +0700

    feat(minibank): add error codes for conflict exception

--- minibank/src/MiniBank.Application/Exceptions/ConflictException.cs
+++ minibank/src/MiniBank.Application/Exceptions/ConflictException.cs
@@ -2,8 +2,8 @@ namespace MiniBank.Application.Exceptions;
 
 public sealed class ConflictException : MiniBankApplicationException
 {
-    public ConflictException(string message)
-        : base(message, 409)
+    public ConflictException(string message, string errorCode = "CONFLICT")
+        : base(message, 409, errorCode)
     {
     }
 }
