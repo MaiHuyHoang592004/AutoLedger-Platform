21b96f503764b86c3bfb4422c10bac6bb5b809cf
HoangMh <hoangmhhe180845@fpt.edu.vn>
2026-03-10 12:54:14 +0700

    feat(minibank): add error codes for internal server exception

--- minibank/src/MiniBank.Application/Exceptions/InternalServerException.cs
+++ minibank/src/MiniBank.Application/Exceptions/InternalServerException.cs
@@ -2,8 +2,8 @@ namespace MiniBank.Application.Exceptions;
 
 public sealed class InternalServerException : MiniBankApplicationException
 {
-    public InternalServerException(string message)
-        : base(message, 500)
+    public InternalServerException(string message, string errorCode = "INTERNAL_SERVER_ERROR")
+        : base(message, 500, errorCode)
     {
     }
 }
