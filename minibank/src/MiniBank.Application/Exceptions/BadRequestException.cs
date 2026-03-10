3dfa5539225997c92000a132bb0e49d3c2b3ee1c
HoangMh <hoangmhhe180845@fpt.edu.vn>
2026-03-10 12:54:10 +0700

    feat(minibank): add error codes for bad request exception

--- minibank/src/MiniBank.Application/Exceptions/BadRequestException.cs
+++ minibank/src/MiniBank.Application/Exceptions/BadRequestException.cs
@@ -2,8 +2,8 @@ namespace MiniBank.Application.Exceptions;
 
 public sealed class BadRequestException : MiniBankApplicationException
 {
-    public BadRequestException(string message)
-        : base(message, 400)
+    public BadRequestException(string message, string errorCode = "BAD_REQUEST")
+        : base(message, 400, errorCode)
     {
     }
 }
