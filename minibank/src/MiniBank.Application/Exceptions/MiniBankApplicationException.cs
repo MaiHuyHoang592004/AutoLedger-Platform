f09d7bd865a40d6eb3fdc0800014624a365c8100
HoangMh <hoangmhhe180845@fpt.edu.vn>
2026-03-10 12:54:08 +0700

    feat(minibank): extend base application exception with error code

--- minibank/src/MiniBank.Application/Exceptions/MiniBankApplicationException.cs
+++ minibank/src/MiniBank.Application/Exceptions/MiniBankApplicationException.cs
@@ -2,11 +2,14 @@ namespace MiniBank.Application.Exceptions;
 
 public abstract class MiniBankApplicationException : Exception
 {
-    protected MiniBankApplicationException(string message, int statusCode)
+    protected MiniBankApplicationException(string message, int statusCode, string errorCode)
         : base(message)
     {
         StatusCode = statusCode;
+        ErrorCode = errorCode;
     }
 
     public int StatusCode { get; }
+
+    public string ErrorCode { get; }
 }
